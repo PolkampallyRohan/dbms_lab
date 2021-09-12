1bf3430a2d61b51fc7b1a3716f4b45da51ad02f7
Polkampally Rohan Rangaraya <72494776+PolkampallyRohan@users.noreply.github.com>
2021-09-12 21:49:33 +0530

    Update assign1.sql

--- lab_1/assign1.sql
+++ lab_1/assign1.sql
@@ -29,37 +29,36 @@ INSERT INTO Campus VALUES('mgt',107,'bom',1500,1,0,1);
 
 
 /*1. Management wants to know the cid of colleges in the uni that can support law.*/
-  SELECT * FROM Campus WHERE law=1;
+  select cid from campus where law=1;
 
 /*2. Faculty wanted to inspect all the details of Dave excluding english and social.*/
- SELECT name,rollno,age,math,sci,sports FROM Student WHERE name='dave';
+  select name,rollno,age,math,sci,sports from student where name="dave";
 
 /*3. Management wants to know the name and roll no of students who have qualified for scholarship.
  Qualification parameter : math OR sci above 80*/
- SELECT name,rollno FROM Student WHERE math>=80 OR sci>=80;
+ select name,rollno from student where math>80 or sci>80;
 
 /*4. A teacher for social wants to know what are the distinct marks she gave her students.*/
- SELECT DISTINCT social FROM Student ;
+ select distinct social from student
 
 /*5. The english prof wishes to find the number of students failing in his class. Students below 70 fail. */
-  SELECT COUNT(*) FROM student WHERE eng<70;
+ select count(*) from student where eng<70;
 
 /*6. The stem field wants to find the best qualified student to be head of their club as per marks. List the
     students name and rollno. (stem is sci and math in this case)*/
-SELECT name,rollno FROM student WHERE math+sci=(SELECT max(math+sci) FROM student);
+ select name,rollno from student where math+sci=(select max(math+sci) from student);
 
 /*7. The Dept. of stem wants to know the college location of the university that does not provide engineering.*/
-SELECT loc FROM campus WHERE engg=0;
+ select loc from campus where engg=0;
 
 /*8. What is the ratio of students in the campus that provides stem and does not provide stem (query and show
   ratio by calc yourself, no need to divide just num/denom)*/
-SELECT (SELECT count(cap) FROM campus WHERE engg=1)/(SELECT count(cap) FROM campus WHERE engg=0);
+ select (select count(cap) from campus where engg=1)/(select count(cap) from campus where engg=0);
 
 /*9. For all rounder find students who have score above 70 in all subj.*/
-SELECT * FROM Student WHERE math>70 AND sci>70 AND eng>70 AND social>70 AND sports>70;
+ select * from student where math>70 and sci>70 and eng>70 and social>70 and sports>70;
 
 /*10. Find the count of students in each group if I split the students as per every 20 roll numbers in one group.*/
- SELECT * FROM Student WHERE rollno>=1 AND rollno<=20;
- SELECT * FROM Student WHERE rollno>=21 AND rollno<=40;
- SELECT * FROM Student WHERE rollno>=41 AND rollno<=60;
-
+ select count(rollno) from student where rollno>1 and rollno<20;
+ select count(rollno) from student where rollno>21 and rollno<40;
+ select count(rollno) from student where rollno>41 and rollno<60;
