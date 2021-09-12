6a27f248013b67dd6b6a770aee9366e04ba70763
Polkampally Rohan Rangaraya <72494776+PolkampallyRohan@users.noreply.github.com>
2021-09-12 21:55:44 +0530

    Update optional.sql

--- lab_1/optional.sql
+++ lab_1/optional.sql
@@ -8,6 +8,7 @@ create table Student(name varchar(20),rollno int,age int ,math int ,sci int,eng 
 insert into Student values ('dwayne',33,21,98,94,91,96,55);
 
 insert into Student values ('john',58,18,70,87,77,98,67); 
+
 insert into Student values ('dave',27,22,54,68,98,96,77);
 
 insert into Student values ('randy',56,19,69,75,65,67,98);
