c0e451325e91800a4ab9da760adf38d6213b9112
Polkampally Rohan Rangaraya <72494776+PolkampallyRohan@users.noreply.github.com>
2021-09-24 21:06:25 +0530

    Update sqlhandler.py

--- lab_3/dbhandler/sqlhandler.py
+++ lab_3/dbhandler/sqlhandler.py
@@ -29,7 +29,7 @@ class Mysqlhandler:
         print(result)
 
 def main():
-  #  obj=  Mysqlhandler()
+   obj=  Mysqlhandler()
   #  obj.add_user('dwayne',3426,'1910-01-19')
   #  obj.add_user('adolf',7868,'2199-09-24')
   #  obj.update_user('adolf','2319-07-30',7868)
