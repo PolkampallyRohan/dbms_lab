02aa5dc18eade2cd9e5180fbbffe27416fda3244
Polkampally Rohan Rangaraya <72494776+PolkampallyRohan@users.noreply.github.com>
2021-09-24 22:30:29 +0530

    Update views.py

--- lab_3/flaskapp/views.py
+++ lab_3/flaskapp/views.py
@@ -1,1 +1,28 @@
+from flask import request
+from . import app
+from dbhandler import Mysqlhandler
+
+ 
+@app.route('/user/register',methods=["POST"])  
+def add():  
+    return Mysqlhandler.add_user("self",request.form['name'],request.form['phno'],request.form['dob']) 
+
+@app.route('/user/update',methods=["POST"])  
+def update():  
+    return Mysqlhandler.update_user("self",request.form['name'],request.form['dob'],request.form['phno']) 
+
+@app.route('/user/delete',methods=["POST"])  
+def delete():  
+    return Mysqlhandler.delete_user("self",request.form['phno'])          
+
+@app.route('/user/view',methods=["GET"])  
+def get_user():  
+    return Mysqlhandler.display_users("self",request.form['name'])  
+
+
+
+
+
+
+
 
